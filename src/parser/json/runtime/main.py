e66e49756895cf0301b1f767d7a897493d6deedb
pscore23 <rubik2357x@gmail.com>
2022-11-02 18:23:27 +0900

    [update] JSON パーサーの完成

--- src/parser/json/runtime/main.py
+++ src/parser/json/runtime/main.py
@@ -1,48 +1,45 @@
-import ast
-import builtins
 import io
-import os
 import re
 import sys
 from typing import Any
 
-from lark import Lark, Transformer, exceptions
+from lark import Lark, Transformer, exceptions, v_args
 
-import _exceptions
+from ._exceptions import ParsingError
+from .define.grammar import _JSON_DG
 
 
 class _TreeToJSON(Transformer):
+    @v_args(inline=True)
     def string(self, string_):
-        (string_,) = string_
+        return string_[1:-1].replace("\\\"", "\"")
 
-        return string_[1:-1]
+    # autopep8: off
+    null = lambda self, _: None
+    true = lambda self, _: True
+    false = lambda self, _: False
+    # autopep8: on
 
-    def number(self, number_):
-        (number_,) = number_
-
-        return float(number_)
-
-    def null(self, _):
-        return None
-
-    def true(self, _):
-        return True
-
-    def false(self, _):
-        return False
-
-    list = list
+    array = list
     pair = tuple
-    dict = dict
+    object = dict
+    number = v_args(inline=True)(float)
 
 
-class JSONParser(object):
+class JSON:
+    def __new__(cls):
+        return super().__new__(cls)
+
     def __init__(self) -> None:
         self._setup()
-
-        with builtins.open(rf"{os.getcwd()}\src\parser\json\define\grammar.lark", "r", encoding="utf-8") as gf:
-            self.grammar = Lark(gf, start="value", parser="lalr", lexer="standard",
-                                propagate_positions=False, maybe_placeholders=False, transformer=_TreeToJSON())
+        self.grammar = Lark(
+            grammar=_JSON_DG,
+            parser="lalr",
+            lexer="standard",
+            propagate_positions=False,
+            maybe_placeholders=False,
+            transformer=_TreeToJSON()
+        )
 
     def _setup(self) -> None:
         sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding="utf-8")
@@ -51,11 +48,10 @@ class JSONParser(object):
 
     def parse(self, __text: str) -> Any:
         try:
-            return ast.literal_eval(str(self.grammar.parse(__text.translate(str.maketrans({"'": "\""})))))
+            return self.grammar.parse(__text.translate(str.maketrans({"'": "\""})))
 
         except exceptions.UnexpectedInput as e:
             hint: list[Any] = re.findall(r"\d+", str(e))[::-1][:2:]
 
-            raise _exceptions.ParsingError(f"""JSON のパースを試みましたが失敗しました:
-                                            理由 - JSON が破損しています
-                                            破損している箇所 - {hint[1]} 行目の {hint[0]} 文字目""")
+            raise ParsingError(
+                f"""JSON corruption: Line {hint[1]}, character {hint[0]}.""")
